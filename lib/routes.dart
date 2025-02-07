df378b26a278b71ecab01ec77e1015e902c0226d
khmuhib01 <khmuhib2013@gmail.com>
2025-02-07 20:17:32 +0600

    testing

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/view/user/UserFormPage.dart';
-import 'package:my_first_app/view/user/UserListPage.dart';
+import 'package:my_first_app/view/user/user_form_page.dart';
 import 'package:my_first_app/view/container_learn.dart';
 import 'package:my_first_app/view/home_page.dart';
 import 'package:my_first_app/view/listview_learn.dart';
 import 'package:my_first_app/view/row_colum.dart';
-import 'package:my_first_app/view/user_page.dart';
 import 'package:my_first_app/controllers/user_controller.dart';
 
 class AppRoutes {
@@ -27,8 +25,8 @@ class AppRoutes {
     container: (context) => ContainerPage(),
     rowColumn: (context) => RowColumPage(),
     listView: (context) => ListViewPage(),
-    user: (context) => UserPage(),
-    userList: (context) => UserListPage(),
+    // user: (context) => UserPage(),
+    // userList: (context) => UserListPage(),
     userForm: (context) => UserFormPage(),
   };
 }
